09b1d46e7a557ff345d88a99e0c46ec16119f48b
Søren Kruse <krusenen@gmail.com>
2017-01-14 18:09:59 +0100

    Add initial unit tests

---
+++ src/Additio.Configuration.Tests/AutoMockedDataAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace Additio.Configuration.Tests
+{
+    internal class AutoMockedDataAttribute : CompositeDataAttribute
+    {
+        public AutoMockedDataAttribute()
+            : this(new AutoNsubstitueDataAttribute())
+        { }
+
+        public AutoMockedDataAttribute(params object[] values)
+            : this(new AutoNsubstitueDataAttribute(), values)
+        { }
+
+        private AutoMockedDataAttribute(AutoNsubstitueDataAttribute autoDataAttributeAttribute, params object[] values)
+            : base(new InlineDataAttribute(values), autoDataAttributeAttribute)
+        { }
+
+        private class AutoNsubstitueDataAttribute : AutoDataAttribute
+        {
+            public AutoNsubstitueDataAttribute()
+                : base(new Fixture().Customize(new AutoConfiguredNSubstituteCustomization()))
+            { }
+        }
+    }
+}
---
+++ src/Additio.Configuration.Tests/DependencyLoaderTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace Additio.Configuration.Tests
+{
+    public class DependencyLoaderTests
+    {
+        [Theory]
+        [AutoMockedData("Sitecore.Analytics", 1)]
+        [AutoMockedData("Sitecore.*", 1)]
+        [AutoMockedData("Sitecore.*, Custom.*", 2)]
+        [AutoMockedData("Sitecore.*, Custom.*.config", 2)]
+        public void GetDependencyPatterns_ShouldAddMissingConfigExtension(string dependencies, int dependencyCount, string file)
+        {
+            // Arrange
+            var attributes = new[] { new XAttribute("dependencies", dependencies) };
+            var attributeLoader = Substitute.For<IAttributeLoader>();
+            attributeLoader.GetAttributesOnRootElement(file).Returns(attributes);
+
+            // Act
+            var loader = new DependencyLoader(attributeLoader);
+            var patterns = loader.GetDependencyPatterns(file).ToList();
+
+            // Assert
+            patterns.Should().HaveCount(dependencyCount);
+            foreach (var pattern in patterns)
+            {
+                pattern.Should().EndWithEquivalent(".config");
+            }
+        }
+    }
+}
