fba9428efb8627ae077889b2f170e232b0bdba47
Søren Kruse <krusenen@gmail.com>
2017-01-14 17:36:51 +0100

    Make node root path variable and add debugger display

--- src/Additio.Configuration/Node.cs
+++ src/Additio.Configuration/Node.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Additio.Configuration
 {
+    [DebuggerDisplay("{RelativePath}")]
     public class Node
     {
-        public const string IncludeFolderPath = @"App_Config\Include";
+        private string Root { get; }
+
+        public Node(string root)
+        {
+            if (!root.EndsWith("\\"))
+                root += "\\";
+            Root = root;
+        }
 
         public string FilePath { get; set; }
 
-        public string RelativePath => FilePath.Remove(0, FilePath.IndexOf(IncludeFolderPath, StringComparison.OrdinalIgnoreCase) + 19);
+        public string RelativePath => FilePath.Replace(Root, "");
 
         public List<Node> Dependencies { get; } = new List<Node>();
     }
